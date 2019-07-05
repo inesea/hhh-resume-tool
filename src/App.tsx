3874673e1c487682955785a3dc902aeb1a413c9a
Inese Avota <inese.avota@gmail.com>
2019-07-05 23:16:19 +0100

    Added first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import MainDetail from './components/MainDetail'
 
-const App = () => <div>CV Template</div>
+const App = () => (
+  <>
+    <MainDetail
+      name="inese avota"
+      phoneNumber="+44 44 444 44444"
+      summaryText="Software engineer trying to build a cv template... Software engineer trying to build a cv template... Software engineer trying to build a cv template... Software engineer trying to build a cv template... Software engineer trying to build a cv template... "
+    />
+  </>
+)
 
 export default App
